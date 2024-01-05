bdd6a73a942bfa0c5ad9b9bb551c5e81b41b9876
kumarmudur <kumarmudur10@gmail.com>
2024-01-05 16:25:31 -0500

    spring boot app initial commit

---
+++ src/test/java/com/kumarmudur/springboot/SpringbootRestfulWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kumarmudur.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestfulWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
